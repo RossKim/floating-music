4660b3b75fd1745542ddefca96b510802bdc4006
mrk13 <mrk13developer@gmail.com>
2021-11-15 21:48:56 +0900

    android 8 overlay permission fix.

--- app/src/main/java/kr/co/mrk13/android/androidfloatingmusic/ui/MainActivity.kt
+++ app/src/main/java/kr/co/mrk13/android/androidfloatingmusic/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package kr.co.mrk13.android.androidfloatingmusic.ui
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.PixelFormat
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
@@ -136,8 +141,33 @@ class MainActivity : AppCompatActivity() {
         dialogDelayMs = 500L
     }
 
+    @Suppress("DEPRECATION")
     private fun checkOverlayDisplayPermission(): Boolean {
-        return Settings.canDrawOverlays(this)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            Settings.canDrawOverlays(this)
+        } else {
+            if (Settings.canDrawOverlays(this)) return true
+            try {
+                val mgr = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                    ?: return false
+                //getSystemService might return null
+                val viewToAdd = View(this)
+                val params = WindowManager.LayoutParams(
+                    0,
+                    0,
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSPARENT
+                )
+                viewToAdd.layoutParams = params
+                mgr.addView(viewToAdd, params)
+                mgr.removeView(viewToAdd)
+                return true
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            false
+        }
     }
 
     private fun requestNotificationPermission() {
